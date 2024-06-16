244b01abf51f70f800423aad05d42dbcc868db31
hopezh <hope.zh@gmail.com>
2024-06-16 21:11:34 +0800

    cmt out opt.fileformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@ local opt = vim.opt
 opt.autoindent = true
 opt.background = "dark"
 opt.colorcolumn = "80" -- highlight the 80th column, nil when virt-column.nvim is used
-opt.fileformat = "unix" -- specify file format to remove the "^M" char at line end
+-- opt.fileformat = "unix" -- specify file format to remove the "^M" char at line end
 
 opt.guicursor =
     "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175" -- blinking cursor
