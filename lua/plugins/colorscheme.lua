9a16dcd4c36f8cc819cc31a9a0258c539d4bee16
hopezh <hope.zh@gmail.com>
2024-06-16 21:10:58 +0800

    add color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+return {
+	-- go to after/plugins/color.lua to activate a color scheme
+	
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+
+		config = function()
+		  -- vim.cmd.colorscheme "catppuccin-mocha"
+		end
+	},
+
+	{
+		"ellisonleao/gruvbox.nvim",
+	},
+
+    {
+        "diegoulloao/neofusion.nvim",
+        priority = 1000,
+
+        opts = ...,
+
+        config = function()
+            require("neofusion").setup({
+                transparent_mode = true,
+            })
+        end,
+    },
+
+}
+
