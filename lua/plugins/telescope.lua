5dd5eeeaf4a133adcb2aeaab9e1e3419bdd964e5
hopezh <hope.zh@gmail.com>
2024-06-17 23:36:33 +0800

    add more keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,10 +10,13 @@ return {
 	config = function()
 		-- set keymaps
 		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'find buffers' })
+		vim.keymap.set('n', '<leader>fc', builtin.command_history, { desc = 'find cmd history' })
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "live grep" })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'find buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'find help tags' })
+		vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'find keymaps' })
+		vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = 'find recent files' })
 
 		require('telescope').setup({
 			-- change some default options
