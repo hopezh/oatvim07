887cfd70d6132ebb9bdab1634d3b43e5b52a620f
hopezh <hope.zh@gmail.com>
2024-06-17 22:46:22 +0800

    set keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,9 +7,46 @@ return {
 		'nvim-lua/plenary.nvim',
 	},
 
+    -- opts = {
+    --     -- change some default options
+    --     defaults = {
+    --         layout_strategy = "horizontal",
+    --         layout_config = {
+    --             prompt_position = "top",
+    --             -- preview_width = 100, -- fix width of preview window
+    --         },
+    --         sorting_strategy = "ascending",
+    --         winblend = 0,
+    --     },
+    --
+    --     pickers = {
+    --         find_files = { hidden = true }, -- show hidden files in search
+    --     },
+    -- },
+
 	config = function()
+		-- set keymaps
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "live grep" })
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'find buffers' })
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'find help tags' })
+
 		require('telescope').setup({
-			---
+			-- change some default options
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = {
+					prompt_position = "top",
+					-- preview_width = 100, -- fix width of preview window
+				},
+				sorting_strategy = "ascending",
+				winblend = 0,
+			},
+
+			pickers = {
+				find_files = { hidden = true }, -- show hidden files in search
+			},
 		})
 	end,
 }
