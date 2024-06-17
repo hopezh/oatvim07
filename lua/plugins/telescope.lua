8de19bcb8494567fe9200eac4a6d801e31d1f945
hopezh <hope.zh@gmail.com>
2024-06-17 23:31:15 +0800

    minor

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,23 +7,6 @@ return {
 		'nvim-lua/plenary.nvim',
 	},
 
-    -- opts = {
-    --     -- change some default options
-    --     defaults = {
-    --         layout_strategy = "horizontal",
-    --         layout_config = {
-    --             prompt_position = "top",
-    --             -- preview_width = 100, -- fix width of preview window
-    --         },
-    --         sorting_strategy = "ascending",
-    --         winblend = 0,
-    --     },
-    --
-    --     pickers = {
-    --         find_files = { hidden = true }, -- show hidden files in search
-    --     },
-    -- },
-
 	config = function()
 		-- set keymaps
 		local builtin = require('telescope.builtin')
