486380620f9ed24e4a27e484d55ac22fd1a2b9ca
hopezh <hope.zh@gmail.com>
2024-06-22 00:04:13 +0800

    add nvim-lspconfig

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		'neovim/nvim-lspconfig',
+		lazy = false,
+
+		config = function()
+			---
+		end,
+	},
+
+	-- {
+	-- 	'hrsh7th/cmp-nvim-lsp',
+	-- 	config = function()
+	-- 		require('cmp').setup({
+	-- 			---
+	-- 		})
+	-- 	end,
+	-- },
+}
