a72b5738833cf5de46a8fd5cd790287e9adc7693
hopezh <hope.zh@gmail.com>
2024-08-03 14:36:47 +0800

    use <leader>ud to toggle diagnostics

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 -- set leader key --------------------------------------------------------------
 vim.g.mapleader = " " -- use the SPACE key as leader key
 
+
 -- use "jk" to ESC -------------------------------------------------------------
 vim.keymap.set("c", "jk", "<ESC>") -- command-line mode
 vim.keymap.set("i", "jk", "<ESC>") -- insert mode
@@ -9,13 +10,39 @@ vim.keymap.set("s", "jk", "<ESC>") -- select mode
 vim.keymap.set("v", "jk", "<ESC>") -- visual & select mode
 vim.keymap.set("x", "jk", "<ESC>") -- visual mode
 
+
 -- in normal mode, use <leader>+n to clear highlight ---------------------------
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
+
 
 -- yank to end of line ---------------------------------------------------------
 vim.keymap.set('n', 'Y', 'y$', { desc = "yank to the end of line"})
 
--- temp ========================================================================
+
+-- toggle disgnostic -----------------------------------------------------------
+-- source: https://github.com/WhoIsSethDaniel/toggle-lsp-diagnostics.nvim
+vim.keymap.set(
+    'n',
+    '<leader>ud',
+    (function()
+        local diag_status = 1 -- 1 is show; 0 is hide
+        return function()
+          if diag_status == 1 then
+            diag_status = 0
+            vim.diagnostic.hide()
+          else
+            diag_status = 1
+            vim.diagnostic.show()
+          end
+        end
+    end)(),
+    { desc = 'toggle diagnostic' }
+)
+
+
+--------------------------------------------------------------------------------
+-- temp
+--------------------------------------------------------------------------------
 
 -- vim-easy-align --------------------------------------------------------------
 -- vim.cmd([[nmap ga <Plug>(EasyAlign)]])
